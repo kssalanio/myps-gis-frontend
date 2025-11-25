7bd4bf66a26a955d6daba188237270e352e8daa5
Ken Salanio <kssalanio@gmail.com>
2025-11-25 09:52:19 +0800

    [#1] init vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr'
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+export default defineConfig(() => ({
+	build: {
+		outDir: '../Main/wwwroot/'
+	},	
+	server: {
+		host: "::",
+		port: 8081,
+	},
+	plugins: [
+		react(),
+		tailwindcss(),
+		svgr({
+			svgrOptions: {
+				exportType: 'default',
+			},
+			include: '**/*.svg?react',
+		}),
+	].filter(Boolean),
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+}))
